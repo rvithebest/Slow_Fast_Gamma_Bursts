dab768c85c004aeba7e77bd449782ab4989ec39c
R VIGNESH <vigneshr1@iisc.ac.in>
2025-10-11 09:34:03 -0700

    Updated codes

--- LFP_analysis/Synthetic_data_codes/mp_analysis_synth_LFP.m
+++ LFP_analysis/Synthetic_data_codes/mp_analysis_synth_LFP.m
@@ -19,7 +19,7 @@ for v = 1 % only once first
         length_accumulator_omp_gear{jj,4} = [length_accumulator_omp_gear{jj,4}, length_gatherer_delta];
         gabor_accmulator_omp_gear{jj,1}=gaborInfo_current;
         %%%%%%%%%%%%%%%% MP %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        length_accumulator_MP{jj,1} = [length_accumulator_MP{jj,1}, length_gatherer_MP]; % \
+        length_accumulator_MP{jj,1} = [length_accumulator_MP{jj,1}, length_gatherer_MP]; % default threshold
         length_accumulator_MP{jj,2} = [length_accumulator_MP{jj,2}, length_gatherer_MP_beta];
         length_accumulator_MP{jj,3} = [length_accumulator_MP{jj,3}, length_gatherer_MP_gamma];
         length_accumulator_MP{jj,4} = [length_accumulator_MP{jj,4}, length_gatherer_MP_delta];
@@ -30,15 +30,17 @@ for v = 1 % only once first
         length_accumulator_hilbert{jj,3} = [length_accumulator_hilbert{jj,3}, length_gatherer_hilbert_gamma];
         length_accumulator_hilbert{jj,4} = [length_accumulator_hilbert{jj,4}, length_gatherer_hilbert_delta];
         %%%%%%%%%%%%%%% Wavelet %%%%%%%%%%%%%%%%%%%%%%%%%%%
-        length_accumulator_wavelet{jj,1} = [length_accumulator_wavelet{jj,1}, length_gatherer_wavelet];
-        length_accumulator_wavelet{jj,2} = [length_accumulator_wavelet{jj,2}, length_gatherer_wavelet_beta];
-        length_accumulator_wavelet{jj,3} = [length_accumulator_wavelet{jj,3}, length_gatherer_wavelet_gamma];
-        length_accumulator_wavelet{jj,4} = [length_accumulator_wavelet{jj,4}, length_gatherer_wavelet_delta];
-        %%%%%%%%%%%%%%% Feingold %%%%%%%%%%%%%%%%%%%%%%%%%%%
-        length_accumulator_feingold{jj,1} = [length_accumulator_feingold{jj,1}, length_gatherer_feingold];
-        length_accumulator_feingold{jj,2} = [length_accumulator_feingold{jj,2}, length_gatherer_feingold_beta];
-        length_accumulator_feingold{jj,3} = [length_accumulator_feingold{jj,3}, length_gatherer_feingold_gamma];
-        length_accumulator_feingold{jj,4} = [length_accumulator_feingold{jj,4}, length_gatherer_feingold_delta];
+        % we do not use wavelet and feingold in our analysis, although the
+        % result is consistent with these methods as well.
+        % length_accumulator_wavelet{jj,1} = [length_accumulator_wavelet{jj,1}, length_gatherer_wavelet];
+        % length_accumulator_wavelet{jj,2} = [length_accumulator_wavelet{jj,2}, length_gatherer_wavelet_beta];
+        % length_accumulator_wavelet{jj,3} = [length_accumulator_wavelet{jj,3}, length_gatherer_wavelet_gamma];
+        % length_accumulator_wavelet{jj,4} = [length_accumulator_wavelet{jj,4}, length_gatherer_wavelet_delta];
+        % %%%%%%%%%%%%%%% Feingold %%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % length_accumulator_feingold{jj,1} = [length_accumulator_feingold{jj,1}, length_gatherer_feingold];
+        % length_accumulator_feingold{jj,2} = [length_accumulator_feingold{jj,2}, length_gatherer_feingold_beta];
+        % length_accumulator_feingold{jj,3} = [length_accumulator_feingold{jj,3}, length_gatherer_feingold_gamma];
+        % length_accumulator_feingold{jj,4} = [length_accumulator_feingold{jj,4}, length_gatherer_feingold_delta];
         %%%%%%%%%%%%%%%% Analong data- Synthetic %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         analogData_accumulator{jj,1}=[analogData_accumulator{jj,1},analogData];
         analogData_accumulator{jj,2}=[analogData_accumulator{jj,2},analogData0];
